215a6eef3dcff020e32b06b5bcc768b2aea27c61
Adrián Moreno Monterde <adrianmorenomonterde@MacBook-Pro-de-Adrian.local>
2022-12-15 10:39:00 +0100

    Insercion de tweets.csv

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # ProyectoFinalAGI
 En este repositorio iremos subiendo el proyecto y sus respectivos actualizaciones
 El archivo README.md se quedará para ir escribiendo notas y decisiones que iremos tomando a lo largo del desarrollo de la aplicación.
+
+**Temática de los tweets** *(no están repartidos equitativamente)*:
+- Qatar2022
+- Ukraine
+- Messi
+- Inflation
+- Health
+- Huelva
+- UHU
+- Recreativo de Huelva
+- Nuevo Colombino
+- USA
+- Goku
+
+En archivo *tweets.json* están los 50k tweets con las diferentes temáticas
+En archivo *Tweets_Streaming_To_MongoDB.py* está el script utilizado para la descarga de los tweets en la base de datos de mongoDB en *localhost*. 
---
+++ Tweets_Streaming_To_MongoDB.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 12 20:26:36 2020
+@author: Jacinto Mata
+"""
+
+import tweepy
+from tweepy import Stream
+import json
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+import datetime
+from keys import *
+
+
+class MiClaseTwitter(Stream):
+
+    def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret, bd, limite):
+        Stream.__init__(self, consumer_key=consumer_key, consumer_secret=consumer_secret, access_token=access_token, access_token_secret=access_token_secret)
+        self.contador = 0
+        self.limite = limite
+        self.coleccion = bd.tweets
+        
+    def on_connection_error(self):
+        self.disconnect()
+    
+    def on_connect(self):
+        print("Conexión correcta!!!!")
+        
+        
+    def on_data(self, data):
+        if self.contador < self.limite:
+
+               
+        # Decode the JSON data from Twitter
+            tweet = json.loads(data)
+       
+            #if tweet['lang'] == 'es':
+            print("Tweet número " ,self.contador)
+            print(tweet['created_at']) 
+            print(tweet['user']['screen_name'])
+            print(tweet['text'])
+            if 'text' in tweet:
+                text = tweet['text']
+            if 'extended_tweet' in tweet:
+                text = tweet['extended_tweet']['full_text']
+            if 'retweeted_status' in tweet:
+                tweet_RT = tweet['retweeted_status']
+                if 'text' in tweet_RT:
+                    text = 'RT @' + tweet['retweeted_status']['user']['screen_name'] + tweet_RT['text']    
+                if 'extended_tweet' in tweet_RT:
+                    extended_RT = tweet_RT['extended_tweet']
+                    text = 'RT @' + tweet['retweeted_status']['user']['screen_name'] + extended_RT['full_text']
+        
+            tweet_id = tweet['id_str']                      # El ID en formato cadena
+            username = tweet['user']['screen_name']         # El autor del tweet
+            followers = tweet['user']['followers_count']    # Número de seguidores
+            hashtags = tweet['entities']['hashtags']        # Los hashtags usados
+            dt = tweet['created_at']                        # El timestamp cuando fue creado
+            language = tweet['lang']                        # El idioma
+
+        	# # Convierte dt en un formato más adecuado para MongoDB
+            created = datetime.datetime.strptime(dt, '%a %b %d %H:%M:%S +0000 %Y')
+
+            tweetSimplificado = {'id':tweet_id, 'username':username, 'followers':followers, 
+                          'text':text, 'hashtags':hashtags, 'language':language, 'created':created }
+
+        
+            self.coleccion.insert_one(tweetSimplificado)
+            #self.coleccion.insert_one(tweet)
+         
+            self.contador += 1
+        else:
+            print('Límite de tweets alcanzado')
+            self.disconnect()
+            
+
+    def on_error(self, status_code):
+        print("Error", status_code)
+
+if __name__ == '__main__':
+
+    # Si queremos conectarnos a un cluster de Atlas, se usa la misma cadena que para conectarse desde los programas (Compass, Studio 3T, etc.)
+    # En este caso se conecta a un cluster "local" en el que uno de los nodos "escucha" por el puerto 27000
+    stringConexion='mongodb://localhost:27017'
+    
+    try:
+
+        cliente = MongoClient(stringConexion)
+        limite = 27944
+        bd = cliente.Twitter
+        MiStream = MiClaseTwitter(consumer_key, consumer_secret, access_token, access_token_secret, bd=bd, limite=limite)
+        MiStream.filter(track = ['Qatar2022' , 'Ukraine' , 'Messi' , 'Inflation' , 'Health', 'Huelva', 'UHU', 'Recreativo de Huelva', 'Nuevo Colombino', 'USA', 'Goku'])
+
+    except ConnectionFailure:
+        print('Failed to connect to replicaSet')
+
